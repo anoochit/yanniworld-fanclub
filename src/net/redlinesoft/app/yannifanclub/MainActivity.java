74f24e0c43c72a5c7ba071334de4a3ae6de06436
redlinesoft <anoochit@gmail.com>
2013-02-24 17:09:16 +0700

    replace ics actionbar with actionbar sherlock

--- src/net/redlinesoft/app/yannifanclub/MainActivity.java
+++ src/net/redlinesoft/app/yannifanclub/MainActivity.java
@@ -18,7 +18,6 @@ import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
-import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
 import android.net.ConnectivityManager;
@@ -27,9 +26,8 @@ import android.net.Uri;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.os.StrictMode;
+import android.support.v4.app.FragmentTransaction;
 import android.util.Log;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
 import android.view.Window;
 import android.widget.AdapterView;
@@ -38,53 +36,52 @@ import android.widget.LinearLayout;
 import android.widget.ListView;
 import android.widget.Toast;
 
+import com.actionbarsherlock.app.ActionBar;
+import com.actionbarsherlock.app.ActionBar.Tab; 
+import com.actionbarsherlock.app.SherlockActivity;
+import com.actionbarsherlock.view.MenuInflater;
+import com.actionbarsherlock.view.MenuItem;
+import com.actionbarsherlock.widget.ShareActionProvider;
 import com.google.ads.AdRequest;
 import com.google.ads.AdSize;
 import com.google.ads.AdView;
 
-public class MainActivity extends Activity {
-
-	private AdView adView;
+public class MainActivity extends SherlockActivity  {
+ 
 	int data_size = 0; 
 	public ArrayList<HashMap<String, String>> MyArrList = new ArrayList<HashMap<String, String>>();
 	public HashMap<String, String> map;
 	ListView listItem;
-
+  
 	String url = "http://query.yahooapis.com/v1/public/yql?q=select%20title.content%2C%20link.href%20from%20feed%20where%20url%3D%22https%3A%2F%2Fgdata.youtube.com%2Ffeeds%2Fapi%2Fusers%2Fyannivideos%2Fuploads%3Fmax-results%3D50%22%20and%20link.rel%3D%22alternate%22&format=json&callback=";
-
+	
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		requestWindowFeature(Window.FEATURE_INDETERMINATE_PROGRESS);
 		setContentView(R.layout.activity_main); 
-		setProgressBarIndeterminateVisibility(false);
-		 
-
+		setProgressBarIndeterminateVisibility(false); 
+		
 		// break policy
 		if (android.os.Build.VERSION.SDK_INT > 9) {
-			StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder()
-					.permitAll().build();
+			StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
 			StrictMode.setThreadPolicy(policy);
-		} 
+		}  
 
 		// get xml data form yql
 		if (checkNetworkStatus()) {
-
 			// Create the adView
-			adView = new AdView(this, AdSize.BANNER, "a15116b3fb5c1da");
+			AdView adView = new AdView(this, AdSize.BANNER, "a15116b3fb5c1da");
 			// Lookup your LinearLayout assuming it’s been given
 			// the attribute android:id="@+id/mainLayout"
 			LinearLayout layout = (LinearLayout) findViewById(R.id.mainLayout);
 			// Add the adView to it
 			layout.addView(adView);
 			// Initiate a generic request to load it with an ad
-			adView.loadAd(new AdRequest());
-
-			new  LoadContentAsync().execute();
-
+			adView.loadAd(new AdRequest());			
+ 			new  LoadContentAsync().execute();
 		} else {
-			Toast.makeText(getBaseContext(), "No network connection!",
-					Toast.LENGTH_SHORT).show(); 
+			Toast.makeText(getBaseContext(), "No network connection!",Toast.LENGTH_SHORT).show(); 
 		}
 
 	}
@@ -104,10 +101,8 @@ public class MainActivity extends Activity {
 				for (int i = 0; i < json_entry.length(); i++) {
 					JSONObject c = json_entry.getJSONObject(i);
 					Log.d("JSON", c.getString("title").toString());
-					Log.d("JSON", c.getJSONObject("link").getString("href")
-							.toString());
-					String link = c.getJSONObject("link").getString("href")
-							.toString();
+					Log.d("JSON", c.getJSONObject("link").getString("href").toString());
+					String link = c.getJSONObject("link").getString("href").toString();
 					String[] fragments = link.split("&");
 					String[] videoid = fragments[0].split("=");
 					Log.d("JSON", videoid[1]);
@@ -117,8 +112,7 @@ public class MainActivity extends Activity {
 					map.put("title", c.getString("title"));
 					map.put("link", c.getJSONObject("link").getString("href"));
 					map.put("videoid", videoid[1]);
-					MyArrList.add(map);
-				 
+					MyArrList.add(map);				 
 				}
 
 				data_size = json_entry.length();
@@ -126,7 +120,6 @@ public class MainActivity extends Activity {
 			} catch (JSONException e) {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
-				Toast.makeText(getBaseContext(), "Cannot connect to server!",Toast.LENGTH_SHORT).show();
 			}
 			return null;
 		}
@@ -154,14 +147,15 @@ public class MainActivity extends Activity {
 			listItem.setAdapter(adapter);				
 			listItem.setOnItemClickListener(new OnItemClickListener() {
 				@Override
-				public void onItemClick(AdapterView<?> arg0, View arg1, int arg2,
-						long arg3) {
+				public void onItemClick(AdapterView<?> arg0, View arg1, int arg2,long arg3) {
 					Intent fanPageIntent = new Intent(Intent.ACTION_VIEW);
 					fanPageIntent.setType("text/url");
 					fanPageIntent.setData(Uri.parse(MyArrList.get(arg2).get("link")));
 					startActivity(fanPageIntent);			
 				}
 			});
+		} else {
+			Toast.makeText(getBaseContext(), "Cannot connect to server!",Toast.LENGTH_SHORT).show();
 		}
 	}
 
@@ -176,8 +170,7 @@ public class MainActivity extends Activity {
 			if (statusCode == 200) { // Download OK
 				HttpEntity entity = response.getEntity();
 				InputStream content = entity.getContent();
-				BufferedReader reader = new BufferedReader(
-						new InputStreamReader(content));
+				BufferedReader reader = new BufferedReader(	new InputStreamReader(content));
 				String line;
 				while ((line = reader.readLine()) != null) {
 					str.append(line);
@@ -195,40 +188,30 @@ public class MainActivity extends Activity {
 
 	public boolean checkNetworkStatus() {
 		ConnectivityManager connectivityManager = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
-		NetworkInfo activeNetworkInfo = connectivityManager
-				.getActiveNetworkInfo();
+		NetworkInfo activeNetworkInfo = connectivityManager.getActiveNetworkInfo();
 		return activeNetworkInfo != null;
-	}
-
+	} 
+	
 	@Override
-	public boolean onOptionsItemSelected(MenuItem item) {
+	public boolean onCreateOptionsMenu(com.actionbarsherlock.view.Menu menu) {
+		// TODO Auto-generated method stub
+		MenuInflater inflater = getSupportMenuInflater();
+		inflater.inflate(R.menu.activity_main, menu);		
+		MenuItem actionItem = menu.findItem(R.id.menu_item_share_action_provider_action_bar);
+        ShareActionProvider actionProvider = (ShareActionProvider) actionItem.getActionProvider();
+        actionProvider.setShareIntent(createShareIntent());		 
+        return true;
+	} 
 
-		switch (item.getItemId()) {
-		case R.id.menu_share:
-			Log.d("MENU", "select menu share");
-			Intent sharingIntent = new Intent(
-					android.content.Intent.ACTION_SEND);
-			sharingIntent.setType("text/*");
-			sharingIntent.putExtra(android.content.Intent.EXTRA_SUBJECT,
-					getString(R.string.text_share_subject));
-			sharingIntent.putExtra(android.content.Intent.EXTRA_TEXT,
-					getString(R.string.text_share_body)
-							+ getApplicationContext().getPackageName());
-			startActivity(Intent.createChooser(sharingIntent,
-					getString(R.string.menu_share)));
-			startActivity(sharingIntent);
-			break;
-		case R.id.menu_update:
-			Log.d("MENU", "select menu update");
-			break;
-		}
-		return false;
-
+	private Intent createShareIntent() {
+		// TODO Auto-generated method stub
+		Intent shareIntent = new Intent(android.content.Intent.ACTION_SEND);
+		shareIntent.setType("text/*");
+		shareIntent.putExtra(android.content.Intent.EXTRA_SUBJECT, getString(R.string.text_share_subject));
+		shareIntent.putExtra(android.content.Intent.EXTRA_TEXT, getString(R.string.text_share_body)	+ getApplicationContext().getPackageName());
+		return shareIntent;
 	}
+ 
+ 	
 
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		getMenuInflater().inflate(R.menu.activity_main, menu);
-		return true;
-	}
 }
